9303a9a91680bc2b14961dfdf5d48dc91c4155d6
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-05 18:07:36 +0530

    boosttrap the project

---
+++ backend/src/index.ts
@@ -0,0 +1,36 @@
+import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+
+
+const app = new Hono<{
+  Bindings:{
+    DATABASE_URL: string
+  }
+}>()
+
+
+app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+}).$extends(withAccelerate())
+  return c.text('Signup')
+})
+
+app.post('/api/v1/signin', (c) => {
+  return c.text('Signin')
+})
+
+app.post('/api/v1/blog', (c) => {
+  return c.text('Blog')
+})
+
+app.put('/api/v1/blog', (c) => {
+  return c.text('Blog-put')
+})
+
+app.get('/api/v1/blog', (c) => {
+  return c.text('Blog-get')
+})
+
+export default app
