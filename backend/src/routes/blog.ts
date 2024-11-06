e7d5ccb004a3bdd81e3ed6f6b55ee559063f42e3
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-07 00:02:25 +0530

    put blog route

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -33,7 +33,27 @@ blogRouter.post("/", async (c) => {
   });
 });
 
+//put -> change the content and title
+blogRouter.put("/",async (c) => {
+    const body = await c.req.json();
+    const prisma = new PrismaClient({
+        datasourceUrl : c.env.DATABASE_URL,
+    }).$extends(withAccelerate())
 
+    const blog = await prisma.blog.update({
+        where: {
+            id : body.id
+        },
+        data: {
+            title : body.title,
+            content : body.content,
+            authorId : 1
+        }
+    })
+  return c.json({
+    id : blog.id
+  });
+});
 
 blogRouter.get("/", (c) => {
   return c.text("Blog-get");
