f537d6b9d0a614dca2bc87fc7fb289b2524213d1
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-07 01:09:15 +0530

    Middleware and set i.e pass the userId

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -1,19 +1,33 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
+import { verify } from "hono/jwt";
 
 export const blogRouter = new Hono<{
   Bindings: {
     DATABASE_URL: string;
     JWT_SECRET: string;
   };
+  Variables: {
+    userId: string;
+  };
 }>();
 
 //for Authentication use Middlewares
 //middleware
-// blogRouter.use("/*", (c, next)=> {
-//     next()
-// })
+blogRouter.use("/*", async (c, next) => {
+  const authHeader = c.req.header("authorization") || "";
+  const user = await verify(authHeader, c.env.JWT_SECRET);
+  if (user) {
+    c.set("userId", String(user.id));
+    return next();
+  } else {
+    c.status(403);
+    return c.json({
+      message: "You are not Authenticated",
+    });
+  }
+});
 
 blogRouter.post("/", async (c) => {
   const body = await c.req.json();
