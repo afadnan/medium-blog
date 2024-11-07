07ca839c7df076c6ce9e3fcb95d01b571637fb1e
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-07 21:13:39 +0530

    upload zod code on npm and install zod package from npm

--- backend/src/routes/blog.ts
+++ backend/src/routes/blog.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import { Hono } from "hono";
 import { verify } from "hono/jwt";
-import { signupInput } from "./zod";
+import { signupInput } from "afadnan-medium-blog-common";
 
 export const blogRouter = new Hono<{
   Bindings: {
