175f5033b8bc8ef2013273362d904cbbc5ad4e91
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-09 03:29:53 +0530

    made it responsive

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -3,10 +3,10 @@ import { Auth } from '../components/Auth'
 
 const Signin = () => {
   return (
-    <div className='grid grid-cols-2 '>
+    <div className='grid grid-cols-1 lg:grid-cols-2 '>
     <Auth type="signin"/>
     
-<div className='invisible lg:visible '>
+<div className='hidden lg:block '>
     <Quote/>
     </div>
 </div>
--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -3,10 +3,10 @@ import { Quote } from '../components/Quote'
 import { Auth } from '../components/Auth'
 const Signup = () => {
     return (
-    <div className='grid grid-cols-2 '>
+    <div className='grid grid-cols-1 lg:grid-cols-2 '>
         <Auth type="signup"/>
         
-    <div className='invisible lg:visible '>
+    <div className='hidden lg:block'>
         <Quote/>
         </div>
     </div>
