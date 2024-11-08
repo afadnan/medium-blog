8382f595f5234dc9014deacf7c391276fa9d8d74
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-08 14:46:25 +0530

    Create an Account

--- frontend/src/pages/Signup.tsx
+++ frontend/src/pages/Signup.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import { Quote } from '../components/Quote'
+import { Auth } from '../components/Auth'
 const Signup = () => {
     return (
-      <div>Signup</div>
+    <div className='grid grid-cols-2 '>
+        <Auth/>
+        
+    <div className='invisible lg:visible '>
+        <Quote/>
+        </div>
+    </div>
     )
   }
   
