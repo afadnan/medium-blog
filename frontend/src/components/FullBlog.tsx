b5a378994aafead4de80cfdac5f189cc186c2389
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-14 02:16:37 +0530

    In FullBlog Author Details,Name done

--- frontend/src/components/FullBlog.tsx
+++ frontend/src/components/FullBlog.tsx
@@ -1,4 +1,5 @@
 import { Appbar } from "./Appbar";
+import { Avatar2 } from "./BlogCard";
 
 export const FullBlog = ({ blog }: { blog: blog }) => {
   return (
@@ -9,13 +10,29 @@ export const FullBlog = ({ blog }: { blog: blog }) => {
           <div className=" col-span-8">
             <div className="text-3xl font-extrabold">{blog.title}</div>
             <div className="text-slate-600 pt-4">
-                Posted on 13th November 2024
+              Posted on 13th November 2024
             </div>
-            <div className="pt-4">
-                {blog.content}
+            <div className="pt-4">{blog.content}</div>
+          </div>
+          <div className="col-span-4">
+            <div className="text-slate-500">
+              Author Details
+              <div className="flex w-full pt-2 ">
+                <div className="pr-4 flex flex-col justify-center">
+                  <Avatar2 name={blog.author.name || "Anonymous"} />
+                </div>
+                <div>
+                    <div className="text-xl font-bold text-black">
+                    {blog.author.name || "Anonymous"}
+                    </div>
+                    <div className="pt-2  text-slate-500">
+                    Random catch phrase about the author's ability to grab the
+                    users's attention
+                    </div>
+              </div>
+              </div>
             </div>
           </div>
-          <div className="bg-green-200 col-span-4">hello</div>
         </div>
       </div>
     </div>
