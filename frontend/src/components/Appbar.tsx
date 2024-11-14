2f7bd111bcdb411e8da7c051557fe13bc9b34bd9
Adnan Ashraf <af.adnan23@gmail.com>
2024-11-15 01:31:23 +0530

    On clicking on the Medium goto the blog page

--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,18 +1,23 @@
-
+import { Link } from "react-router-dom";
 
 export const Appbar = () => {
-  return <div className="py-4 border-b px-20 justify-between flex text-2xl font-bold">
-    <div className="flex flex-col justify-center">Medium</div>
-    <div><Avatar name="Adnan"/></div>
-
-  </div>;
+  return (
+    
+    <div className="py-4 border-b px-20 justify-between flex text-2xl font-bold">
+      <Link to="/blogs" className="flex flex-col justify-center">Medium</Link>
+    <div>
+      <Avatar name="Adnan" />
+    </div>
+    </div>
+  );
 };
 
 export function Avatar({ name }: { name: string }) {
-    return (
-    <div className="relative inline-flex items-center justify-center w-8 h-8 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600 ">
+  return (
+    <div className="flex items-center justify-center w-7 h-7  bg-gray-100 rounded-full dark:bg-gray-600 ">
       <span className="font-xs text-gray-600 dark:text-gray-300 ">
         {name[0]}
       </span>
-    </div>);
-  }
+    </div>
+  );
+}
